473fedacf040352eac332440e52fd62512c8389d
Juan Eduardo <juaneduardodelgado@gmail.com>
2021-12-10 08:58:55 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {WelcomeComponent} from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'app',
+  component: HomeComponent,
+}, {
+  path: '**',
+  component: WelcomeComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { GridComponent } from './components/grid/grid.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WelcomeComponent,
+    GridComponent,
+    LogoComponent,
+    HeroComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
